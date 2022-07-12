f7e38b28ddb23067289958d6f57af11f856f0447
Егор Гущин <gea11@yandex.ru>
2022-07-12 12:28:31 +0300

    add serializers

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,10 @@
+from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework import filters
 
 from categories.models import Category, Genre, Title
+from .serializers import (CategorySerializer, GenreSerializer,
+                          TitleSerializer)
 
 class CategoryViewSet():
     queryset = Category.objects.all()
@@ -8,3 +12,17 @@ class CategoryViewSet():
     permission_classes = 
     filter_backend = (filters.SearchFilter)
     search_fields = ('name',)
+
+class GenreViewSet():
+    queryset = Genre.objects.all()
+    serializers_class = GenreSerializer
+    permission_classes = 
+    filter_backend = (filters.SearchFilter)
+    search_fields = ('name',)
+
+class TitleViewSet():
+    queryset = Title.objects.all()
+    serializers_class = TitleSerializer
+    permission_classes = 
+    filter_backend = (DjangoFilterBackend)
+    filterset_fields = ('category', 'genre', 'name', 'year')
