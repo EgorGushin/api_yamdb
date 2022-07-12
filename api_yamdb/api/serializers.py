fe0174bec2379cee84336d06eeb2ca6f729d2db7
UnRainbow <ranmadm@gmail.com>
2022-07-12 12:07:59 +0300

    serializer added

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -5,7 +5,7 @@ from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    username = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
         fields = '__all__'
