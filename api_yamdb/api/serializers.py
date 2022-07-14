2d305d0e840860dcfea6c400a00476edaa9d77d3
UnRainbow <101391679+UnRainbow@users.noreply.github.com>
2022-07-14 04:24:46 +0300

    Update serializers.py

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -13,14 +13,6 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
 
 
-# class UserEditSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         fields = ("username", "email", "first_name",
-#                   "last_name", "bio", "role")
-#         model = User
-#         read_only_fields = ('role',)
-
-
 class GetConfirmationCodeSerializer(serializers.ModelSerializer):
     username = serializers.CharField(
         validators=(UniqueValidator(queryset=User.objects.all()), ),
