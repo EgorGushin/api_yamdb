6b69632bd190ac6b916c39e29e8f1fbbf735c032
Сергей Деревянченко <sergey.derevyanchenko@yandex.ru>
2022-07-14 15:35:19 +0300

    serielizers fixed

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
+from rest_framework.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
 
 from reviews.models import Category, Comment, Genre, Review, Title, User
 
@@ -57,6 +59,17 @@ class ReviewSerializer(serializers.ModelSerializer):
         read_only=True,
         default=serializers.CurrentUserDefault()
     )
+
+    def validate(self, data):
+        request = self.context['request']
+        author = request.user
+        title_id = self.context['view'].kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if request.method == 'POST':
+            if Review.objects.filter(title=title, author=author).exists():
+                raise ValidationError('Вы уже оставляли отзыв.')
+        return data
+    
 
     class Meta:
         model = Review
